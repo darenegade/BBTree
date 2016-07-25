a7b26b4c131beeb7cfa015d64261ac5bd960a6db
René Zarwel <darenegade@gmx.de>
2016-07-25 14:11:04 +0200

    Init

---
+++ BBTree.cpp
@@ -0,0 +1,34 @@
+//
+// Created by René Zarwel on 25.07.16.
+//
+#include "BBTree.h"
+
+bool BBTree::Node::search(const int) const {
+    return false;
+}
+
+Node *BBTree::Node::insert(const int, const double) {
+    return nullptr;
+}
+
+Node *BBTree::Node::remove(const int, const double) {
+    return nullptr;
+}
+
+
+bool BBTree::search(const int) const {
+    return false;
+}
+
+void BBTree::insert(const int) {
+
+}
+
+void BBTree::remove(const int) {
+
+}
+
+
+
+
+
